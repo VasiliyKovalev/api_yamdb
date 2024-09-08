f123624f2a6b4ac82e46d23d7702ea007cd4e1c5
Василий Ковалев <kovalev177vasiliy@mail.ru>
2024-09-08 07:49:49 +0300

    users

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,34 +1,45 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+
+from .utils import validate_username_not_me
+
+
+MAX_LENGTH_USERNAME = 150
+MAX_LENGTH_USER_ROLE = 20
 
 
 class User(AbstractUser):
     """Пользовательская модель User."""
-    USER = 'user'
-    MODERATOR = 'moderator'
-    ADMIN = 'admin'
-    ROLE_CHOICES = (
-        (USER, 'user'),
-        (MODERATOR, 'moderator'),
-        (ADMIN, 'admin'),
+    class UserRole(models.TextChoices):
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+    username = models.CharField(
+        'Никнейм',
+        max_length=MAX_LENGTH_USERNAME,
+        unique=True,
+        validators=(UnicodeUsernameValidator(), validate_username_not_me)
     )
-    email = models.EmailField('Электронная почта', max_length=254, unique=True)
+    email = models.EmailField('Электронная почта', unique=True)
     bio = models.TextField('Биография', blank=True)
     role = models.CharField(
         'Пользовательская роль',
-        max_length=20,
-        choices=ROLE_CHOICES,
-        default=USER
+        max_length=MAX_LENGTH_USER_ROLE,
+        choices=UserRole.choices,
+        default=UserRole.USER
     )
-    confirmation_code = models.IntegerField(null=True, blank=True)
 
     class Meta:
-        constraints = [
-            models.CheckConstraint(
-                check=~models.Q(username='me'),
-                name='username_not_me'
-            )
-        ]
+        ordering = ('id',)
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
-        return self.username
+        return f'Пользователь: {self.username}'
+
+    def is_moderator(self):
+        return self.role == self.UserRole.MODERATOR
+
+    def is_admin(self):
+        return self.is_superuser or (self.role == self.UserRole.ADMIN)
